9c2268a0d78458eaacabcf303b31bd95bbf0f861
nadir <snadir177@gmail.com>
2024-11-19 00:27:20 -0500

    update main.go

--- internal/ports/secondary.go
+++ internal/ports/secondary.go
@@ -10,8 +10,8 @@ type TmuxPort interface {
 	HasSession(ctx context.Context) (int, error)
 	NewSession(ctx context.Context) error
 	AttachSession(ctx context.Context) error
-	SendKeys(ctx context.Context) error
-	NewWindow(ctx context.Context) error
+	SendKeys(ctx context.Context, windowName string, keyCmd string) error
+	NewWindow(ctx context.Context, name string) error
 	SelectWindow(ctx context.Context) error
 	SetOption(ctx context.Context) error
 	SetWindowOpt(ctx context.Context) error
