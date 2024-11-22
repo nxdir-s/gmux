4e2011d1a8876197a7992f9d42dad6dbd63667e3
nxdir-s <snadir177@gmail.com>
2024-11-22 12:18:42 -0500

    rename service field

--- internal/core/service/tmux.go
+++ internal/core/service/tmux.go
@@ -7,35 +7,35 @@ import (
 )
 
 type TmuxService struct {
-	adapter ports.TmuxPort
+	tmux ports.TmuxPort
 }
 
 func NewTmuxService(adapter ports.TmuxPort) (*TmuxService, error) {
 	return &TmuxService{
-		adapter: adapter,
+		tmux: adapter,
 	}, nil
 }
 
 func (s *TmuxService) SessionExists(ctx context.Context) int {
-	return s.adapter.HasSession(ctx)
+	return s.tmux.HasSession(ctx)
 }
 
 func (s *TmuxService) NewSession(ctx context.Context, name string) error {
-	return s.adapter.NewSession(ctx, name)
+	return s.tmux.NewSession(ctx, name)
 }
 
 func (s *TmuxService) NewWindow(ctx context.Context, cfgIndex int) error {
-	return s.adapter.NewWindow(ctx, cfgIndex)
+	return s.tmux.NewWindow(ctx, cfgIndex)
 }
 
 func (s *TmuxService) SelectWindow(ctx context.Context, cfgIndex int) error {
-	return s.adapter.SelectWindow(ctx, cfgIndex)
+	return s.tmux.SelectWindow(ctx, cfgIndex)
 }
 
 func (s *TmuxService) SendKeys(ctx context.Context, name string, args ...string) error {
-	return s.adapter.SendKeys(ctx, name, args...)
+	return s.tmux.SendKeys(ctx, name, args...)
 }
 
 func (s *TmuxService) AttachSession(ctx context.Context) error {
-	return s.adapter.AttachSession(ctx)
+	return s.tmux.AttachSession(ctx)
 }
