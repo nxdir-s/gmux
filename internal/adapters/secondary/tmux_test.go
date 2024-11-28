22e7a88787a04b8df21f31b83dbb909da04b6025
nxdir-s <snadir177@gmail.com>
2024-11-28 01:28:30 -0500

    add unit test for TmuxAdapter.NewSession

--- internal/adapters/secondary/tmux_test.go
+++ internal/adapters/secondary/tmux_test.go
@@ -13,9 +13,13 @@ import (
 )
 
 const (
-	TmuxSessionName      string = "TmuxUnitTests"
+	TmuxSessionName string = "TmuxUnitTests"
+	TmuxWindowName  string = "TmuxUnitTests"
+
 	TmuxSessionExists    string = "HasSession should return 0 if a tmux session exists"
 	TmuxSessionNotExists string = "HasSession should return 1 if a tmux session does not exists"
+
+	TmuxNewSession string = "NewSession should create a new tmux session"
 )
 
 func TestHasSession(t *testing.T) {
@@ -61,7 +65,49 @@ func TestHasSession(t *testing.T) {
 			}
 
 			if out := adapter.HasSession(ctx); out != tc.expected {
-				t.Errorf("error got %d, want %d", out, tc.expected)
+				t.Errorf("got %d, want %d", out, tc.expected)
+			}
+		})
+	}
+}
+
+func TestNewSession(t *testing.T) {
+	var cases = []struct {
+		name      string
+		cfg       *entity.Config
+		expected  error
+		shouldErr bool
+	}{
+		{
+			name: TmuxNewSession,
+			cfg: &entity.Config{
+				Session: TmuxSessionName,
+			},
+			expected:  nil,
+			shouldErr: false,
+		},
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, tmux.Alias, string(tmux.NewSessionCmd), "-d", "-s", TmuxSessionName, "-n", TmuxWindowName)
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			mockCmd, err := tests.NewCommandMock(tc.cfg, cmd, tc.shouldErr)
+			if err != nil {
+				t.Errorf("failed to created CommandMock: %s", err.Error())
+			}
+
+			adapter, err := NewTmuxAdapter(tc.cfg, mockCmd)
+			if err != nil {
+				t.Errorf("failed to created TmuxAdapter: %s", err.Error())
+			}
+
+			if out := adapter.NewSession(ctx, TmuxWindowName); out != tc.expected {
+				t.Errorf("got %d, want %d", out, tc.expected)
 			}
 		})
 	}
