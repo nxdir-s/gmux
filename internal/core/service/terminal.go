2a05f1d65eb3374d60f7eb99489c130580d4f6bd
nxdir-s <snadir177@gmail.com>
2025-07-12 16:31:21 -0400

    cleanup

--- internal/core/service/terminal.go
+++ internal/core/service/terminal.go
@@ -16,11 +16,6 @@ func NewTerminal(adapter ports.Tmux) *Terminal {
 	}
 }
 
-// TmuxSessionExists checks if a tmux session exists
-func (s *Terminal) TmuxSessionExists(ctx context.Context, session string) int {
-	return s.tmux.HasSession(ctx, session)
-}
-
 // NewTmuxSession creates a new session
 func (s *Terminal) NewTmuxSession(ctx context.Context, name string) error {
 	return s.tmux.NewSession(ctx, name)
@@ -31,7 +26,12 @@ func (s *Terminal) NewTmuxWindow(ctx context.Context, session string, name strin
 	return s.tmux.NewWindow(ctx, session, name)
 }
 
-// SelectTmuxWindow selects a tmux window
+// TmuxSessionExists checks if a tmux session exists
+func (s *Terminal) TmuxSessionExists(ctx context.Context, session string) int {
+	return s.tmux.HasSession(ctx, session)
+}
+
+// TmuxSelectWindow selects a tmux window
 func (s *Terminal) TmuxSelectWindow(ctx context.Context, session string, window string) error {
 	return s.tmux.SelectWindow(ctx, session, window)
 }
@@ -41,7 +41,7 @@ func (s *Terminal) TmuxSendKeys(ctx context.Context, cmd []string, session strin
 	return s.tmux.SendKeys(ctx, cmd, session, window)
 }
 
-// AttachSession attempts to attach to a tmux session
+// TmuxAttachSession attempts to attach to a tmux session
 func (s *Terminal) TmuxAttachSession(ctx context.Context, session string) error {
 	return s.tmux.AttachSession(ctx, session)
 }
