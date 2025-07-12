24bc63a415a8a1ccba9b6cc533d2d91ef841bdad
nxdir-s <snadir177@gmail.com>
2025-07-12 16:29:03 -0400

    cleanup

--- internal/ports/core.go
+++ internal/ports/core.go
@@ -11,9 +11,9 @@ type Terminal interface {
 }
 
 type TerminalService interface {
-	TmuxSessionExists(ctx context.Context, session string) int
 	NewTmuxSession(ctx context.Context, name string) error
 	NewTmuxWindow(ctx context.Context, session string, name string) error
+	TmuxSessionExists(ctx context.Context, session string) int
 	TmuxSelectWindow(ctx context.Context, session string, window string) error
 	TmuxSendKeys(ctx context.Context, cmd []string, session string, window string) error
 	TmuxAttachSession(ctx context.Context, session string) error
